d878281c8e5393a1bcc966b3d02b4d05b37786fb
unknown <joseantonioibanezrobles@gmail.com>
2018-10-03 13:49:37 +0200

    Muestra el nombre, la ciudad de nacimiento y la localidad actual

--- Prueba.java
+++ Prueba.java
@@ -1,6 +1,7 @@
 public class Prueba{
   public static void main(String args[]){
    System.out.println("Jose Antonio Ibañez Robles");
-   System.out.println("León");
+   System.out.println("león");
+   System.out.println("Villabalter");
  }
 }
